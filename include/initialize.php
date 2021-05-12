e1730d519ff4deef627622eebb6781cbe73b5097
kwabena-Koranteng <60141919+kwabena-Koranteng@users.noreply.github.com>
2021-05-12 13:45:31 +0000

    Update initialize.php

--- include/initialize.php
+++ include/initialize.php
@@ -4,25 +4,25 @@
 
 //DIRECTORY_SEPARATOR is a PHP Pre-defined constants:
 //(\ for windows, / for Unix)
-defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+// defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
-defined('SITE_ROOT') ? null : define ('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'].DS.'jobportal');
+// defined('SITE_ROOT') ? null : define ('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'].DS.'jobportal');
 
-defined('LIB_PATH') ? null : define ('LIB_PATH',SITE_ROOT.DS.'include');
+// defined('LIB_PATH') ? null : define ('LIB_PATH',SITE_ROOT.DS.'include');
 
 //load the database configuration first.
 require_once("../include/config.php");
-require_once(LIB_PATH.DS."function.php");
-require_once(LIB_PATH.DS."session.php");
-require_once(LIB_PATH.DS."accounts.php");
-require_once(LIB_PATH.DS."autonumbers.php");  
-require_once(LIB_PATH.DS."companies.php");  
-require_once(LIB_PATH.DS."job.php");  
-require_once(LIB_PATH.DS."employees.php");  
-require_once(LIB_PATH.DS."categories.php");  
-require_once(LIB_PATH.DS."applicant.php");  
-require_once(LIB_PATH.DS."jobregistration.php");  
+require_once"../include/function.php");
+require_once("../include/session.php");
+require_once("../include/accounts.php");
+require_once(."../include/autonumbers.php");  
+require_once("../include/companies.php");  
+require_once(."../include/job.php");  
+require_once("../include/employees.php");  
+require_once("../include/categories.php");  
+require_once("../include/applicant.php");  
+require_once("../include/jobregistration.php");  
   
 
-require_once(LIB_PATH.DS."database.php");
+// require_once(LIB_PATH.DS."database.php");
 ?>
