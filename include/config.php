e506b0e75e7a90edc5a8975f0fa25addbd042317
kwabena koranteng <kwabena.obeng@ashesi.edu.gh>
2021-05-12 12:55:40 +0000

    Make it better

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
-defined('server') ? null : define("server", "localhost");
-defined('user') ? null : define ("user", "root") ;
-defined('pass') ? null : define("pass","");
-defined('database_name') ? null : define("database_name", "db_jobportal") ;
+defined('server') ? null : define("server", "us-cdbr-east-03.cleardb.com");
+defined('user') ? null : define ("user", "bea28d40262729") ;
+defined('pass') ? null : define("pass","ce3bbc12");
+defined('database_name') ? null : define("database_name", "heroku_f7d2d42a143fdb0") ;
 
 $this_file = str_replace('\\', '/', __File__) ;
 $doc_root = $_SERVER['DOCUMENT_ROOT'];
