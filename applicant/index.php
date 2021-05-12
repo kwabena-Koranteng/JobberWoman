a354aa118d409de511aa3ed99bb4d9fafb7ee8e9
kwabena-Koranteng <60141919+kwabena-Koranteng@users.noreply.github.com>
2021-05-12 14:21:30 +0000

    Update index.php

--- applicant/index.php
+++ applicant/index.php
@@ -1,5 +1,5 @@
 <?php 
-require_once("../include/initialize.php");  
+require_once("./include/initialize.php");  
 if (!isset($_SESSION['APPLICANTID'])) {
 	# code...
 	redirect(web_root.'index.php');
